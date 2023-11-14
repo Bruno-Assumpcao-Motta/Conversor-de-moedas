0f04557d6b8bf02c7f4de170a6ab9907fd413846
Bruno-Assumpcao-Motta <bruno.assump@gmail.com>
2023-11-14 19:05:11 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,10 @@ var valorEmReal = valorEmDolar * cotacaoDolar;
 valorEmReal = valorEmReal.toFixed(2);
 
 alert("R$ " + valorEmReal);
+
+var valorEmEuro = 50;
+var cotacaoEuro = 5.29;
+var valorEmEuro = valorEmEuro * cotacaoEuro;
+valorEmEuro = valorEmEuro.toFixed(2);
+
+alert(`${"A Cotação do euro é EUR $: " + valorEmReal}`)
