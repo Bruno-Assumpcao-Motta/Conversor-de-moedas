c2418199bf140c4dca675da13b7ebdfa542abd07
Bruno-Assumpcao-Motta <bruno.assump@gmail.com>
2023-11-14 18:35:32 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,6 @@
-var valorEmDolar = 30;
-alert(valorEmDolar);
+var valorEmDolar = 64;
+var cotacaoDolar = 5.32;
+var valorEmReal = valorEmDolar * cotacaoDolar;
+valorEmReal = valorEmReal.toFixed(2);
+
+alert("R$ " + valorEmReal);
